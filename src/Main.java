d506fe1433a9047130c3103c483c89efd35e35de
Agnese <agnese.fomrate@gmail.com>
2023-03-06 19:04:01 +0200

    Created calculator

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Exercise 7");
+        int sum = 5 + 8;
+        System.out.println("Sum of 5 and 8 is = " + sum);
+
+        int difference = 11 - 4;
+        System.out.println("Difference of 11 and 4 is: " + difference);
+
+        int product = 5 * 8;
+        System.out.println("Product of 5 and 8 is: " + product);
+
+        double quotient = 5.0/8.5;
+        System.out.println("Quotient of 5.0 and 8.5 is: " + quotient);
+
+
+    }
+}
