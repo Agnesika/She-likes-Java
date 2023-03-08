6700373cfe264b836db16c2880451a3d59636a67
Agnese <agnese.fomrate@gmail.com>
2023-03-08 21:06:14 +0200

    float numbers ranges

--- src/com/sda/she_likes_java/numbers/FloatingPointRanges.java
+++ src/com/sda/she_likes_java/numbers/FloatingPointRanges.java
@@ -9,5 +9,12 @@ public class FloatingPointRanges {
         System.out.println("float number");
         System.out.println("minimal float number is: " + Float.MIN_VALUE);
         System.out.println("maximum float number is: " + Float.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("double number");
+        System.out.println("minimal double number is: " + Double.MIN_VALUE);
+        System.out.println("maximum double number is: " + Double.MAX_VALUE);
+
     }
 }
