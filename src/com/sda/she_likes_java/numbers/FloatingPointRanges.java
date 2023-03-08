73aaef27cba73c38d0da841d11234f72914cf6b4
Agnese <agnese.fomrate@gmail.com>
2023-03-08 21:01:32 +0200

    ranges of float numbers explained

---
+++ src/com/sda/she_likes_java/numbers/FloatingPointRanges.java
@@ -0,0 +1,13 @@
+package com.sda.she_likes_java.numbers;
+
+// names of the class should start with capital letter - always
+// names of the variable should start with lower case letter :)
+public class FloatingPointRanges {
+
+    public static void main(String[] args) {
+        System.out.println("Now floating numbers...");
+        System.out.println("float number");
+        System.out.println("minimal float number is: " + Float.MIN_VALUE);
+        System.out.println("maximum float number is: " + Float.MAX_VALUE);
+    }
+}
