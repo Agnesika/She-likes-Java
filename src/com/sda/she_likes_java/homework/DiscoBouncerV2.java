59898a75a995ce6f0f2e9d9be8cec0d6f20b2f60
Agnese <agnese.fomrate@gmail.com>
2023-03-10 20:18:03 +0200

    The name issue corrected

--- src/com/sda/she_likes_java/homework/DiscoBouncerV2.java
+++ src/com/sda/she_likes_java/homework/DiscoBouncerV2.java
@@ -3,27 +3,26 @@ package com.sda.she_likes_java.homework;
 import java.util.Scanner;
 
 public class DiscoBouncerV2 {
-    public static void main(String[] args) {
-        System.out.println("Party time?");
 
-        System.out.println("Somebody is coming to the club");
-        System.out.println("Hi, how old are you?");
+    public static void main(String[] args) {
+        System.out.println("Party time???");
+
+        System.out.println("Somebody is coming to club");
+        System.out.println("Hi, how old are You: ");
 
         Scanner inputReader = new Scanner(System.in);
-        String declineMessage = "Sorry, go home :) ";
-
+        String declineMessage = "Sorry, go home:)";
         int age = inputReader.nextInt();
 
         if (age >= 18) {
-            System.out.println("OK, so you are an adult");
-            System.out.println("Let me know your height");
-            double height = inputReader.nextDouble();
+            System.out.println("Ok, so You're adult:)");
+            System.out.println("Let me know Your height: ");
+            int height = inputReader.nextInt();
             if (height >= 200) {
-                System.out.println("Great! Welcome to the party!");
+                System.out.println("Great!!!! Welcome to the party:)");
             } else {
-                System.out.println("declineMessage");
+                System.out.println(declineMessage);
             }
-
         } else {
             System.out.println(declineMessage);
         }
