b598d25bad1fe323498306cea42497ff6544c1bd
Agnese <agnese.fomrate@gmail.com>
2023-03-13 18:30:00 +0200

    Created new file for upgraded Disco Bouncer

---
+++ src/com/sda/she_likes_java/homework/DiscoBouncerUpgradeNew.java
@@ -0,0 +1,27 @@
+package com.sda.she_likes_java.homework;
+
+import java.util.Scanner;
+
+public class DiscoBouncerUpgradeNew {
+
+    public static void main(String[] args) {
+        System.out.println("Party time???");
+
+        System.out.println("Somebody is coming to club");
+        System.out.println("Hi, how old are You: ");
+
+        Scanner inputReader = new Scanner(System.in);
+        String declineMessage = "Sorry, you don't comply with the rules for youngsters :)";
+        int age = inputReader.nextInt();
+        System.out.println("Let me know Your height now: ");
+        double height = inputReader.nextDouble();
+        boolean isAllowedToParty = age >= 18 && age <=30 && height <= 1.5;
+
+        if (isAllowedToParty) {
+            System.out.println("Yeah. Welcome to the club of singing liliputs!");
+            System.out.println("We are happy to see and hear you today");
+        } else {
+            System.out.println(declineMessage);
+        }
+    }
+}
