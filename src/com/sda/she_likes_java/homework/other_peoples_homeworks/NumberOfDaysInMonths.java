9945232b0a439d3238ff67ff6873f559ee1be1c6
Agnese <agnese.fomrate@gmail.com>
2023-03-18 00:08:10 +0200

    Florinas example of days in months with february

---
+++ src/com/sda/she_likes_java/homework/other_peoples_homeworks/NumberOfDaysInMonths.java
@@ -0,0 +1,36 @@
+package com.sda.she_likes_java.controls;
+
+import java.util.Scanner;
+
+public class NumberOfDaysInMonths {
+    public static void main(String[] args) {
+        System.out.println("Hey! Tell me the number of the month:");
+        Scanner inputReader = new Scanner(System.in);
+        int monthNumber = inputReader.nextInt();
+        switch (monthNumber) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("There are 31 days in this month");
+                break;
+            case 2:
+                System.out.println("what Year is it?");
+                int year = inputReader.nextInt();
+                if (year % 4 == 0) {
+                    System.out.println("there are 29 days this year");
+                } else {
+                    System.out.println("There are 28 days in this month");
+                }
+                break;
+            case 4, 6, 9, 11:
+                System.out.println("There are 30 days in this month");
+                break;
+            default:
+                System.out.println("Don't cheat!");
+        }
+    }
+}
