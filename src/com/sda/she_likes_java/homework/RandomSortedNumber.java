d2457bc5bb76008ce5a5585998d094d2680c4d87
Agnese <agnese.fomrate@gmail.com>
2023-03-20 18:20:01 +0200

    Added Random number sorting homework

---
+++ src/com/sda/she_likes_java/homework/RandomSortedNumber.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.util.UUID.Holder.numberGenerator;
+
+public class RandomSortedNumber {
+
+
+    public static void main(String[] args) {
+        Random numberGenerator = new Random();
+
+        int firstNumber = numberGenerator.nextInt();
+        System.out.println("Random number provided by java is: " + firstNumber);
+
+        int secondNumber = numberGenerator.nextInt();
+        System.out.println("Random number provided by java is: " + secondNumber);
+
+        int thirdNumber = numberGenerator.nextInt();
+        System.out.println("Random number provided by java is: " + thirdNumber);
+
+
+
+
+
+    }
+
+}
