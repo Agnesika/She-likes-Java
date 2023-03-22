5769c8af843f8275445803589f0c4064b7699e3b
Agnese <agnese.fomrate@gmail.com>
2023-03-22 21:33:52 +0200

    Classes - introduction

---
+++ src/com/sda/she_likes_java/classes/Person.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.classes;
+
+public class Person {
+    String name = "Agnese";
+    String surname = "Fomrate";
+    int age = 40;
+    String address = "planet Earth";
+}
