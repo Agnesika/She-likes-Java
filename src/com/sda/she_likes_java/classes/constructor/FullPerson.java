3c0d9b19d510145f1dd63cd27f43b55175fd89a4
Agnese <agnese.fomrate@gmail.com>
2023-03-27 21:12:19 +0300

    Static method first touch

--- src/com/sda/she_likes_java/classes/constructor/FullPerson.java
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -1,5 +1,7 @@
 package com.sda.she_likes_java.classes.constructor;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class FullPerson {
     String name;
     String surname;
@@ -27,10 +29,23 @@ public class FullPerson {
 
 
     }
+public void describeYourself() {
+        System.out.println("Hi, I am " + name);
+    }
+    public static void genericDescription() {
+        System.out.println("Generic description");
+    }
 
     public static void main(String[] args) {
         FullAddress address = new FullAddress("Italy", "Roma", "La Curraza", "1162");
         FullPerson person = new FullPerson("Agnese", "Fomrate", 41, address);
         System.out.println("Person is: " + person);
+
+        person.describeYourself();
+
+        FullPerson personWithoutAddress = new FullPerson("Maria", "Kowalska", 34, null);
+        personWithoutAddress.describeYourself();
+
+        genericDescription();
     }
 }
