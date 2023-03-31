d3f71d59a2ee83028ea1e203a386b5e2be6025a2
Agnese <agnese.fomrate@gmail.com>
2023-03-31 16:27:31 +0300

    Created exercise 16 with 3 classes about owner and animal

---
+++ src/com/sda/she_likes_java/homework/exercise_16/Homework.java
@@ -0,0 +1,33 @@
+package com.sda.she_likes_java.homework.exercise_16;
+
+public class Homework {
+    /*
+    Exercise 16Create a hierarchy of classes:
+
+    use a separate package: exercise_16
+    new Owner class with private fields
+    First name
+    Surname,
+    Animal
+
+class Animal containing the fields
+
+    first name
+    age
+    breed
+
+Use private variables and create get/set methods
+Create a toString() method for each class.
+Create a main method and a sample class hierarchy. (edited)
+     */
+
+    public static void main(String[] args) {
+
+
+        Owner maybeMe = new Owner("Agnese", "The Driver", "The Cat");
+        Animal maybeShe = new Animal("Chiepa", 5, "Tricolor hunter");
+        System.out.println("Some info about me " + maybeMe);
+        System.out.println("And some about my dear friend " + maybeShe);
+
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise_16/Owner.java
@@ -0,0 +1,35 @@
+package com.sda.she_likes_java.homework.exercise_16;
+
+public class Owner {
+    private String name;
+    private String occupation;
+    private String animal;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getOccupation() { return occupation; }
+
+    public void setOccupation(String occupation) {this.occupation = occupation; }
+
+    public String getAnimal() { return animal; }
+
+    public void setAnimal(String animal) { this.animal = animal; }
+
+    public Owner(String name, String occupation, String animal) {
+        this.name = name;
+        this.occupation = occupation;
+        this.animal = animal;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "name='" + name + '\'' +
+                ", occupation='" + occupation + '\'' +
+                ", animal='" + animal + '\'' +
+                '}';
+    }
+}
+
