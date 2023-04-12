f60cfdcb7288e2033c1446dc5256cacae23d1ac1
Agnese <agnese.fomrate@gmail.com>
2023-04-12 23:25:12 +0300

    Florinas book homework added

---
+++ src/com/sda/she_likes_java/homework/other_peoples_homeworks/Florinas_homework/Book.java
@@ -0,0 +1,57 @@
+package com.sda.she_likes_java.homework.other_peoples_homeworks.Florinas_homework;
+
+public class Book {
+
+    private String author, title;
+    private int yearOfPublication;
+
+    private static int numberOfBooks;
+
+    public Book(String author, String title, int yearOfPublication) {
+        this.author = author;
+        this.title = title;
+        this.yearOfPublication = yearOfPublication;
+        numberOfBooks++;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public static int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public static void setNumberOfBooks(int numberOfBooks) {
+        Book.numberOfBooks = numberOfBooks;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                '}';
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/other_peoples_homeworks/Florinas_homework/Random.java
@@ -0,0 +1,5 @@
+package com.sda.she_likes_java.homework.other_peoples_homeworks.Florinas_homework;
+
+public class Random {
+
+}
