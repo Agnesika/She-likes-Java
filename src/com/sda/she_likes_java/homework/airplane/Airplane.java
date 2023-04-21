67c7b1908ac4e47174ccabccde589b289aea29ae
Agnese <agnese.fomrate@gmail.com>
2023-04-21 18:49:16 +0300

    Created airplane package

---
+++ src/com/sda/she_likes_java/homework/airplane/Airplane.java
@@ -0,0 +1,9 @@
+package com.sda.she_likes_java.homework.airplane;
+
+public class Airplane {
+    private String brand;
+    private String model;
+    private int seats;
+    private int speed;
+
+}
---
+++ src/com/sda/she_likes_java/homework/airplane/Runway.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.homework.airplane;
+
+public class Runway {
+    private int xCoords;
+    private int yCoords;
+    private int length;
+
+}
