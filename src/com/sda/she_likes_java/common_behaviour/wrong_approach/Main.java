ebd488f396d72ed67dd8115470250d0bcc4ba98a
Agnese <agnese.fomrate@gmail.com>
2023-04-26 21:29:16 +0300

    Moveable interface introduced and main class updated

--- src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
@@ -8,5 +8,33 @@ public class Main {
 
         Animal tiger = new Tiger();
         tiger.go();
+
+        Bird parrot = new Parrot();
+        Bird ostrich = new Ostrich();
+
+        Flyable superman = new Superman();
+        Flyable flyableParrot = new Parrot();
+
+        Swimmable tiger2 = (Swimmable) tiger;
+        Swimmable swimmableSuperman = (Swimmable) superman;
+        Swimmable swimmableTiger = new Tiger();
+
+        Superman superSuperMan = new Superman();
+        superSuperMan.fly();
+        superSuperMan.swim();
+        Flyable superFly = superSuperMan;
+        superFly.fly();
+        Swimmable superSwim = superSuperMan;
+
+        // Tiger assignment ways
+        Tiger newTiger = new Tiger();
+        newTiger.go();
+        newTiger.swim();
+        Animal animalTiger = new Tiger();
+        animalTiger.go();
+        Swimmable swimmableTiger2 = new Tiger();
+        swimmableTiger2.swim();
+        Object objectTiger = new Tiger();
+        // cannot call go() and swim() - thery are not visible here
     }
 }
--- src/com/sda/she_likes_java/common_behaviour/wrong_approach/Superman.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Superman.java
@@ -1,13 +1,18 @@
 package com.sda.she_likes_java.common_behaviour.wrong_approach;
 
-public class Superman implements Flyable, Swimmable {
+public class Superman implements Flyable, Swimmable, Moveable {
     @Override
     public void fly() {
-        System.out.println("Superman is flying");
+        System.out.println("Superman is flying!!!");
     }
 
     @Override
     public void swim() {
         System.out.println("Superman is super swimmer");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Superman is moving super fast");
     }
 }
