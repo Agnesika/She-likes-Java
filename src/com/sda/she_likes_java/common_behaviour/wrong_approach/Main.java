35bde99253cfa92af9446e96d77dc6dd6fbe8ba2
Agnese <agnese.fomrate@gmail.com>
2023-04-26 20:57:31 +0300

    Interfaces example

---
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.common_behaviour.wrong_approach;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+//        bird.fly();
+
+        Animal tiger = new Tiger();
+        tiger.go();
+    }
+}
