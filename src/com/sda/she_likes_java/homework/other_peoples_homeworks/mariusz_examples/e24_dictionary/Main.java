c78f0bc0b0a04f63cc49049dda12fc1193cf7654
Agnese <agnese.fomrate@gmail.com>
2023-05-13 00:12:18 +0300

    Added Mariusz example of dictionary homework

---
+++ src/com/sda/she_likes_java/homework/other_peoples_homeworks/mariusz_examples/e24_dictionary/Main.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.homework.other_peoples_homeworks.mariusz_examples.e24_dictionary;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Let's learn some Polish");
+
+        EnglishPolishDictionary englishPolishDictionary = new EnglishPolishDictionary();
+        englishPolishDictionary.translate("sky");
+        englishPolishDictionary.translate("school");
+        englishPolishDictionary.translate("dog");
+    }
+}
