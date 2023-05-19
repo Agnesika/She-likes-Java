03160a0074debdc1098a8684d07a78717f322727
Agnese <agnese.fomrate@gmail.com>
2023-05-19 17:56:12 +0300

    Created homework nr 26 - important Events

---
+++ src/com/sda/she_likes_java/homework/e26_Events/ImportantEvents.java
@@ -0,0 +1,25 @@
+package com.sda.she_likes_java.homework.e26_Events;
+
+public enum ImportantEvents {
+    GRADUATION(29/02/2006, "Been there-done that, tick the box"),
+    SUMMER_SOLSTICE(21/06/2023, "Yes! That's the way, aha-aha, I like it"),
+    FUNERAL(21/06/2103, "Scatter my ash in the mountains"),
+    JAVA_TEST(18/05/2023, "Everything is an experience");
+
+
+    int eventDate;
+    String description;
+
+    public int getEventDate() {
+        return eventDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    ImportantEvents(int eventDate, String description) {
+        this.eventDate = eventDate;
+        this.description = description;
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/e26_Events/Main.java
@@ -0,0 +1,36 @@
+package com.sda.she_likes_java.homework.e26_Events;
+/*
+ * Create an enumeration class named ImportantEvents.
+ * The class should contain the following fields:
+ * - Date of the event
+ * - Description of the event.
+ * Create at least 3 instances of the class to symbolise your important events.
+ * Create the class Main and in it:
+ * - call the values() method of your class
+ * - write out the contents of the created array (dates and description)
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Let's check my event schedule: ");
+        ImportantEvents previousEvent = ImportantEvents.JAVA_TEST;
+        System.out.println("My previous event: " + previousEvent);
+        System.out.println("I can say that " + previousEvent.getDescription());
+        System.out.println("---------------------------");
+        ImportantEvents oldEvent = ImportantEvents.GRADUATION;
+        System.out.println("My old event: " + oldEvent);
+        System.out.println("That's all about: " + oldEvent.getDescription());
+        System.out.println("Shall I say that it was too long ago and expired?");
+        System.out.println("---------------------------");
+        ImportantEvents nextEvent = ImportantEvents.SUMMER_SOLSTICE;
+        System.out.println("My next event: " + nextEvent);
+        System.out.println("How? " + nextEvent.getDescription());
+        System.out.println("---------------------------");
+        ImportantEvents lastEvent = ImportantEvents.FUNERAL;
+        System.out.println("My last event of my physical life: " + lastEvent);
+        System.out.println("Instructions for everyone: " + lastEvent.getDescription());
+
+
+
+    }
+}
