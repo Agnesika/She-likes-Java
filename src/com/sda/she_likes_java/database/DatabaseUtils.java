065b2698ef136c5826076759e6898ee4fd0ee068
Agnese <agnese.fomrate@gmail.com>
2023-05-26 21:33:41 +0300

    App for getting data from test DB - almost working

--- src/com/sda/she_likes_java/database/DatabaseUtils.java
+++ src/com/sda/she_likes_java/database/DatabaseUtils.java
@@ -5,6 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtils {
+    /**
+     Script for creating table
+     * CREATE TABLE TEST
+     * (ID INT PRIMARY KEY, NAME VARCHAR(255));
+     *
+     * Script for inserting one record (row) into table
+     * INSERT INTO
+     * TEST(ID, NAME)
+     * VALUES(1, 'Olga');
+     *
+     * Script for reading data
+     * SELECT ID, NAME
+     * FROM TEST;
+     *
+     * Script for updating data
+     * UPDATE TEST -- name of table to update
+     * SET NAME = 'Alma' -- column value to change
+     * WHERE ID=2; -- selected row
+     *
+     * script for deleting data
+     * DELETE
+     * FROM TEST
+     * WHERE ID=4
+     *
+     */
 
     // handle internally
     public static Connection getConnection() {
--- src/com/sda/she_likes_java/homework/e27_my_date/ComparingMyDateByMonth.java
+++ src/com/sda/she_likes_java/homework/e27_my_date/ComparingMyDateByMonth.java
@@ -1,9 +1,6 @@
 package com.sda.she_likes_java.homework.e27_my_date;
 
-import java.time.Month;
-import java.util.Comparator;
-
-public class ComparingMyDateByMonth implements Comparator<Month> {
+//public class ComparingMyDateByMonth implements Comparator<Month> {
 
 
 //    public int compare(Object o1, Object o2) {
@@ -12,8 +9,8 @@ public class ComparingMyDateByMonth implements Comparator<Month> {
 //        return firstMonth - secondMonth;
 //    }
 
-    @Override
-    public int compare(Month o1, Month o2) {
-        return 0;
-    }
-}
+//    @Override
+//    public int compare(Month o1, Month o2) {
+//        return 0;
+//    }
+//}
