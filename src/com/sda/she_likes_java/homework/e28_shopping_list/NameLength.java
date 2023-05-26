3ce064e1bb284dd5ccb67f6b18c26f80b9f6e153
Agnese <agnese.fomrate@gmail.com>
2023-05-26 18:20:22 +0300

    trying to make it work with external Comparator

--- src/com/sda/she_likes_java/homework/e28_shopping_list/NameLength.java
+++ src/com/sda/she_likes_java/homework/e28_shopping_list/NameLength.java
@@ -1,16 +1,27 @@
 package com.sda.she_likes_java.homework.e28_shopping_list;
 
-//import static com.sun.tools.classfile.Module_attribute.RequiresEntry.length;
+import java.util.Comparator;
 
-//public class NameLength implements Comparator<ShoppingList> {
+//import static com.sun.tools.classfile.Module_attribute.ProvidesEntry.length;
+
+public class NameLength implements Comparator<ShoppingList> {
 
 
-//    @Override
-//    public int compare(ShoppingList o1, ShoppingList o2) {
+    //    @Override
+//    public int compare(String products1, String products2) {
 //        String firstProduct = o1.getProducts(String.valueOf(length));
 //        String secondProduct = o2.getProducts(String.valueOf(length));
-//        return firstProduct - secondProduct;
+//        return Integer.compare(products1.length(), products2.length());
 
 
 //        }
-//}
+
+        @Override
+        public int compare (ShoppingList o1, ShoppingList o2){
+            boolean length = false;
+            String products1 = o1.getProducts(String.valueOf(length));
+            String products2 = o2.getProducts(String.valueOf(length));
+            return Integer.compare(products1.length(), products2.length());
+        }
+    }
+
