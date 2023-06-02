f82123f3c02214a6eafe5b4120bce876d567fab5
Agnese <agnese.fomrate@gmail.com>
2023-06-02 20:41:56 +0300

    Runtime exceptions (unchecked) vs checked ones

---
+++ src/com/sda/she_likes_java/exceptions/RuntimeExample.java
@@ -0,0 +1,33 @@
+package com.sda.she_likes_java.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RuntimeExample {
+
+    public static void main(String[] args) {
+        try {
+//            throwRuntimeException();
+            String name = "bubu";
+            name.length();
+        } catch (MyRuntimeException e) {
+            System.out.println("here");
+        }
+
+        try {
+            int[] numbers = new int[4];
+            numbers[99] = 1;
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+    }
+
+    public static void throwRuntimeException() throws MyRuntimeException {
+        throw new MyRuntimeException("Do I need to handle it???");
+
+    }
+    public static void unexpectedNull() {
+        Map<String, String> map = new HashMap<>();
+        String value = map.get("some-key");
+        System.out.println(value != null ? value.length() : 0);
+    }
+}
