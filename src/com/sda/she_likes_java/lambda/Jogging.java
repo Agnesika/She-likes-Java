e58511e962b014653ea5720ffa40fdc79a3b3f8d
Agnese <agnese.fomrate@gmail.com>
2023-06-05 20:14:02 +0300

    Lambda - default methods

--- src/com/sda/she_likes_java/lambda/Jogging.java
+++ src/com/sda/she_likes_java/lambda/Jogging.java
@@ -1,7 +1,16 @@
 package com.sda.she_likes_java.lambda;
 
+// Classes from java.lang are always imported
+// import java.lang.FunctionalInterface;
 // API - Application programming interface
 // contract - what to do
+@FunctionalInterface
 public interface Jogging {
     void run();
+
+    // simple/default implementation
+    default void walk() {}
+
+    default void drink(String drink) {}
+
 }
